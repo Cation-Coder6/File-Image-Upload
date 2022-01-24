ddba5d3501a93c0917036f88e3af55e41ceadeeb
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-01-25 03:23:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+
+const fileUpload = require('express-fileupload');
+const cloudinary = require("cloudinary").v2; //dont forget to add .v2 or it wont work.
+cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUD_API_KEY,
+        api_secret: process.env.CLOUD_API_SECRET,
+    })
+    // database
+const connectDB = require('./db/connect');
+
+
+//product route
+const productRouter = require("./routes/productRoutes");
+
+
+app.use(express.static("./public"))
+app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
+
+
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.get('/', (req, res) => {
+    res.send('<h1>File Upload Starter</h1>');
+});
+
+
+app.use("/api/v1/products", productRouter)
+
+
+// middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
